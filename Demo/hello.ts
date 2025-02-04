bcd4783db08a6c9786995d6f5119d4cb1f2bf1aa
0trifix <otrifix@proton.me>
2025-02-04 09:37:18 +0100

    Verschillen tussen TS en JS in het aanmaken van variabelen.

--- Demo/hello.ts
+++ Demo/hello.ts
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+let firstName = "Daoud"; //Javascript
+var wrong = "Gebruik dit nooit"; //Deze manier gebruiken we niet meer, we gebruiken let
+
+let firstName2: string = "Daoud"; //Typescript
+let amount: number = 100;
+let lastName: string;
+lastName = "El Hachimi";
+lastName = "Bok";
