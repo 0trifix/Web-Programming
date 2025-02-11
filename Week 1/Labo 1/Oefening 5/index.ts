12abbb836f3b75ce0a90acfec14b89cf0c9f681c
0trifix <otrifix@proton.me>
2025-02-11 09:53:20 +0100

    Begonnen aan oefening 5

---
+++ Week 1/Labo 1/Oefening 5/index.ts
@@ -0,0 +1,9 @@
+import {questionFloat} from "readline-sync";
+
+let bedrag:number = questionFloat("Geef het bedrag in: ")
+let soort_geld:number[] = [500, 200, 100, 50, 20, 10, 5, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+
+for (let index = 0; index < soort_geld.length; index++) {
+  const element = soort_geld[index];
+  
+}
