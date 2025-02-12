d8da23892f90cfe3eb7fc82aa6740630d1a3c439
0trifix <otrifix@proton.me>
2025-02-12 15:39:08 +0100

    aanpassing oefening 2

--- Week 2/Labo 2/oefening 2/index.ts
+++ Week 2/Labo 2/oefening 2/index.ts
@@ -1,18 +1,22 @@
-function add(getal1:number, getal2:number) 
-{
- return getal1 + getal2
-}
-
-function substract(getal1:number, getal2:number) {
-  return getal1 - getal2
-}
-
-function multiply(getal1:number, getal2:number = 1) {
-  return getal1 * getal2
-}
-
-function divide(getal1:number, getal2:number = 1) {
-  return getal1 / getal2
-}
+//function add(getal1:number, getal2:number) 
+//{
+// return getal1 + getal2
+//}
+//
+//function substract(getal1:number, getal2:number) {
+//  return getal1 - getal2
+//}
+//
+//function multiply(getal1:number, getal2:number = 1) {
+//  return getal1 * getal2
+//}
+//
+//function divide(getal1:number, getal2:number = 1) {
+//  return getal1 / getal2
+//}
+const add = (getal1:number , getal2:number) => getal1+getal2;
+const substract = (getal1:number , getal2:number) => getal1-getal2;
+const multiply = (getal1:number , getal2:number = 1) => getal1*getal2;
+const divide = (getal1:number , getal2:number = 1) => getal1/getal2;
 
 console.log(divide(multiply(add(4,5), substract(6,3)),2))
